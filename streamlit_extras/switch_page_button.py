77d740505b467b628d15dcaa362527423d263d4d
macky337 <macky337@gmail.com>
2025-05-25 18:44:20 +0900

    fix:requirements.txtを6

--- streamlit_extras/switch_page_button.py
+++ streamlit_extras/switch_page_button.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 def switch_page(page_name: str):
     """Streamlit標準のマルチページ切り替えを行う"""
-    # ページ名をクエリパラメータに設定して再実行
-    st.experimental_set_query_params(page=page_name)
-    st.experimental_rerun()
+    # ページ名をクエリパラメータに設定して終了（次回読み込みで反映）
+    st.set_query_params(page=page_name)
+    st.stop()
