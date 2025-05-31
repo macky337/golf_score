d6544d26c957703f3976fb60002f9f5e2419fe4f
macky337 <macky337@gmail.com>
2025-05-31 15:53:09 +0900

    fix:不要ファイルを削除した

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name="streamlit_extras",
     version="0.7.1",
     packages=find_packages(),
-)from setuptools import setup, find_packages
+)
+
+from setuptools import setup, find_packages
 
 setup(
     name="golf_score",
