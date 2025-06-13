6b93fa4b14bc3690ee71945eb51e10d32844fd88
macky337 <macky337@gmail.com>
2025-06-14 08:05:57 +0900

    "🗑️ 64個のファイルを削除 (2025/06/14 08:05)"

--- streamlit_extras/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="streamlit_extras",
-    version="0.1.0",
-    packages=find_packages(include=["streamlit_extras", "streamlit_extras.*"]),
-    author="Local Override",
-    description="Local implementation of streamlit_extras utilities",
-)
